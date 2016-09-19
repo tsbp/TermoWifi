0f0c321e905bfc1e399927808c8c019e2a82ca1b
tsbp <tsbp82@gmail.com>
2016-09-19 17:44:12 +0300

    cfg added

--- TermoWifi/ConfigsWindow.xaml.cs
+++ TermoWifi/ConfigsWindow.xaml.cs
@@ -7,7 +7,9 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using System.Collections.Generic;
+using System.Data;
+using System.Collections;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +17,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace TermoWifi
 {
@@ -23,6 +26,20 @@ namespace TermoWifi
 	/// </summary>
 	public partial class ConfigsWindow : Window
 	{
+		public ObservableCollection<User> items = new ObservableCollection<User>();
+		 
+		public static string  tempReturn;
+		public static string  timeReturn;
+		
+		public class User
+        {
+                public string PicTime { get; set; }
+                public string Time { get; set; }
+                public string PicTemp { get; set; }
+                public string Temp { get; set; }
+//                public Button btn;
+        }
+		//==============================================================
 		public ConfigsWindow()
 		{
 			InitializeComponent();
@@ -30,13 +47,80 @@ namespace TermoWifi
 		//==============================================================
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
-						
-		}	
+					
+//				List<User> items = new List<User>();
+				lvConfigs.ItemsSource = items;
+//				items.Add(new User() {  Time = "18:44",   Temp = "25.4"});
+//				items.Add(new User() {  Time = "22:27",   Temp = "21.8"});
+				//items.Add(new User() { PicTime = new Image { Width = 16, Height = 16, Source = new BitmapImage(new Uri("pack://application:,,,/TermoWifi;component/drawable/timeicon.png")) }, Time = "12:24",   Temp = "23.4"});
+				
+//				
+//				ListBoxItem lbItem = new ListBoxItem();
+//				lbItem.Content = "123";
+//				lbConfigs.Items.Add(lbItem);
+		}					
+//		//==============================================================
+//		private void lvButton_Click(object sender, RoutedEventArgs e)
+//		{            			
+//			FrameworkElement fe = (FrameworkElement)sender;
+//			object it = fe.DataContext;
+//			int aa = lvConfigs.Items.IndexOf(it);
+//			
+//	        string sTemp = null, sTime = null;
+//	        
+//			ConfigsWindow.User a = it as ConfigsWindow.User;
+//			if (a!=null)
+//			{
+//				sTemp = a.Temp;
+//				sTime = a.Time;
+//			}
+//			
+//			PartConfig cWin = new PartConfig();		
+//			cWin.lblTemp.Content = sTemp;
+//			cWin.lblTime.Content = sTime;
+//			cWin.Show();
+//
+//		}
+		
 		//==============================================================
 		private void CloseButton_Click(object sender, RoutedEventArgs e)
 		{
 			Close();
 		}
 		//==============================================================
+		void Button_Click(object sender, RoutedEventArgs e)
+		{
+			items.Add(new User() {
+			          	PicTime = "/TermoWifi;component/drawable/timeicon.png",
+			          	Time = "12:24", 
+			          	PicTemp = "/TermoWifi;component/drawable/tempicon.png", 
+			          	Temp = "28,2"});
+		}
+		//==============================================================
+		int currentItem;
+		//==============================================================
+		void lvConfigs_SelectionChanged(object sender, MouseButtonEventArgs e)
+		{
+			ListView fe = (ListView)sender;
+			
+	        string sTemp = null, sTime = null;
+	        currentItem = fe.SelectedIndex;
+	        
+	        sTemp = items[fe.SelectedIndex].Temp;
+	        sTime = items[fe.SelectedIndex].Time;	      
+			
+			PartConfig cWin = new PartConfig();		
+			cWin.lblTemp.Content = sTemp;
+			cWin.lblTime.Content = sTime;
+			cWin.Closed += cWinClosed;
+			cWin.Show();
+		}
+		//==============================================================
+		public void cWinClosed(object sender, System.EventArgs e)
+        {           
+            items[currentItem].Temp = tempReturn;
+            items[currentItem].Time = timeReturn;
+            lvConfigs.Items.Refresh();
+        }
 	}
 }
---
+++ TermoWifi/PartConfig.xaml.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Voodoo
+ * Date: 16.09.2016
+ * Time: 14:11
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace TermoWifi
+{
+	/// <summary>
+	/// Interaction logic for PartConfig.xaml
+	/// </summary>
+	public partial class PartConfig : Window
+	{
+		
+		    
+		public float tempValue;
+		
+		public PartConfig()
+		{
+			InitializeComponent();
+		}
+		//==============================================================
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			float a = float.Parse(lblTemp.Content.ToString());
+			slTemp.Value = a - 19;
+			
+			string b = lblTime.Content.ToString();
+			slTimeH.Value = double.Parse(b.Substring(0, b.IndexOf(":"))) / 2.4;			
+			slTimeM.Value = double.Parse(b.Substring(b.IndexOf(":") + 1)) / 5.9;
+		}
+		//==============================================================
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			ConfigsWindow.tempReturn = lblTemp.Content.ToString();
+			ConfigsWindow.timeReturn = lblTime.Content.ToString();
+			Close();
+		}
+		//==============================================================
+		void slValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+						
+			lblTemp.Content = String.Format("{0,4:N1}", 19 + slTemp.Value);
+		}
+		//==============================================================
+		void slTimeHourChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			string a = lblTime.Content.ToString();
+			string min =  a.Substring(a.IndexOf(":"));
+			double hour = slTimeH.Value * 2.3;		
+			string s = "";		
+			if(hour < 10) 				s += "0";			
+			lblTime.Content = s + (int)hour + min;
+		}
+		//==============================================================
+		void slTimeMinChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			string a = lblTime.Content.ToString();
+			string hour =  a.Substring(0, a.IndexOf(":")+1);
+			double min = slTimeM.Value * 5.9;
+			
+			string s = "";
+			if(min < 10) 				s += "0";
+			lblTime.Content = hour + s + (int)min;
+		}
+	}
+}
